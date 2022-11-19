e8cda7e517c45bc240d78b6b59d460794053fff7
Skye Carroll <skyecarroll42@gmail.com>
2022-11-19 19:47:04 +0000

    Basic UI

--- Assets/Managers/MenuManager.cs
+++ Assets/Managers/MenuManager.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class MenuManager : MonoBehaviour {
+public class MenuManager : MonoBehaviour
+{
     public static MenuManager Instance;
 
-    [SerializeField] private GameObject _selectedHeroObject,_tileObject,_tileUnitObject;
-
-    void Awake() {
+    [SerializeField] private GameObject _selectedHeroObject, _tileObject, _tileUnitObject;
+    [SerializeField] private TextMeshProUGUI _money;
+    [SerializeField] private GameObject[] _Sets;
+    private GameObject currentSet = null;
+    [SerializeField] private Button closeObjects;
+    void Awake()
+    {
         Instance = this;
     }
 
-    public void ShowTileInfo(Tile tile) {
+    public void ShowSet(int setIndex)
+    {
+        if (currentSet != null) { currentSet.SetActive(false);}
+        _Sets[setIndex].SetActive(true);
+        currentSet = _Sets[setIndex];
+        if (!closeObjects.interactable) closeObjects.interactable = true;
+    }
+
+    public void CloseSet()
+    {
+        currentSet.SetActive(false);
+        closeObjects.interactable = false;
+    }
+
+    public void ShowTileInfo(Tile tile)
+    {
 
         if (tile == null)
         {
@@ -24,14 +45,17 @@ public class MenuManager : MonoBehaviour {
         _tileObject.GetComponentInChildren<Text>().text = tile.TileName;
         _tileObject.SetActive(true);
 
-        if (tile.OccupiedObject) {
+        if (tile.OccupiedObject)
+        {
             _tileUnitObject.GetComponentInChildren<Text>().text = tile.OccupiedObject.ObjectName;
             _tileUnitObject.SetActive(true);
         }
     }
 
-    public void ShowSelectedHero(BaseAnimal hero) {
-        if (hero == null) {
+    public void ShowSelectedHero(BaseAnimal hero)
+    {
+        if (hero == null)
+        {
             _selectedHeroObject.SetActive(false);
             return;
         }
