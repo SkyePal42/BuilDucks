3dff845fd74e2033f374ba03c6fbc624b0891686
Skye Carroll <skyecarroll42@gmail.com>
2022-11-19 17:36:27 +0000

    Refactored Tile System

--- Assets/Tiles/Tile.cs
+++ Assets/Tiles/Tile.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour {
+    public enum TileTypes
+    {
+        Grass = 0,
+        Water = 1,
+        Fence = 2,
+        Gate = 3
+    }
+    public TileTypes _tileType;
     public string TileName;
     [SerializeField] protected SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
-    [SerializeField] private bool _isWalkable;
-    [SerializeField] private bool _isSwimmable;
 
     public BaseUnit OccupiedUnit;
     public BaseObject OccupiedObject;
-    public bool Walkable => _isWalkable && (OccupiedObject == null || OccupiedObject.walk);
-    public bool Swimmable => _isSwimmable && (OccupiedObject == null || OccupiedObject.swim);
+    public bool Walkable => _tileType == TileTypes.Grass && (OccupiedObject == null || OccupiedObject.walk);
+    public bool Swimmable => _tileType == TileTypes.Water && (OccupiedObject == null || OccupiedObject.swim);
 
 
-    public virtual void Init(int x, int y)
+    public void Init(int x, int y)
     {
       
     }
