fb2dfd2077375174b0308ebf8c2081438a8e788d
LQClayton <lulu.clayton@yahoo.com>
2022-11-20 00:27:54 +0000

    Adding scripts for objects

---
+++ Assets/_Objects/BinObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinObject : BaseObject
+{
+     public override int Judge()
+    // return int val of how good the placement of the object is
+    {
+        return look;
+    }
+}
