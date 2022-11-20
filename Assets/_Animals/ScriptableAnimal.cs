6074b68a28b31c3d7f2b54babb5faa16b15842fb
LQClayton <lulu.clayton@yahoo.com>
2022-11-20 10:35:42 +0000

    Small fix to buttons

--- Assets/_Animals/ScriptableAnimal.cs
+++ Assets/_Animals/ScriptableAnimal.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// ducks walking on tiles that contain objects
 [CreateAssetMenu(fileName = "New Unit",menuName = "Scriptable Animal")]
 public class ScriptableAnimal : ScriptableObject {
     public BaseAnimal AnimalPrefab;
