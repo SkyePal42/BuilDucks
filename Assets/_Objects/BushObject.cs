d095ecfad59b8991ecaa5363dca4f324d2deb835
LQClayton <lulu.clayton@yahoo.com>
2022-11-20 01:02:59 +0000

    Trees added

--- Assets/_Objects/BushObject.cs
+++ Assets/_Objects/BushObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BenchObject : BaseObject
+public class BushObject : BaseObject
 {
      public override int Judge()
     // return int val of how good the placement of the object is
--- Assets/_Objects/TreeObject.cs
+++ Assets/_Objects/TreeObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BenchObject : BaseObject
+public class TreeObject : BaseObject
 {
      public override int Judge()
     // return int val of how good the placement of the object is
